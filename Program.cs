f1e960aca006a55428b3d4316a1ca434ee5c044b
Oleg Iraklionov <irakliov@mail.ru>
2022-12-20 18:32:41 +0300

    Case10 is ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!"); 
+﻿// Напишите программу, на входе трехзначное число,а на выходе 2 цифра
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine(N / 10 % 10);
